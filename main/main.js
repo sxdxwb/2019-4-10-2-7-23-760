30bf3fd0dfbc882ac253f71499322d51b4b4016b
weibo <654170872@qq.com>
2019-05-26 20:49:20 +0800

    success

--- main/main.js
+++ main/main.js
@@ -1,4 +1,34 @@
 module.exports = function main(inputs) {
+    var moeny = 0;
+    //1接续输入成对象
+    // var taxi = JSON.parse(inputs);
+    //2计算公里价格
+    var mdistance = getDistanceMoney(inputs.distance);
+    //3计算停车时间及停车价格
+    var mparktime = getParkMoney(inputs.parkTime);
+    //４计算总价(四舍五入)
+    moeny += mdistance + mparktime;
     // write your code here...
-    return "price";
+    return Math.round(moeny);
 };
+
+function getDistanceMoney(distance) {
+    var money = 0;
+    if(distance<=2)
+    {
+        money = 6;
+    }
+    else if(distance<=8)
+    {
+        money += 6 + (distance-2)*0.8;
+    }
+    else if(distance>8)
+    {
+        money += 6 + 6*0.8 + (distance-8)*1.2;
+    }
+    return money;
+}
+
+function getParkMoney(parktime) {
+    return  0.25 * parktime;
+}
